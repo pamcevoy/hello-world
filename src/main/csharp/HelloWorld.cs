1b227f7ee0432f06c1bb36a27f5dbd571839f916
Patrick McEvoy <pmcevoy12@yahoo.com>
2019-02-20 20:24:34 -0500

    csharp implementation

---
+++ src/main/csharp/HelloWorld.cs
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2019 Patrick McEvoy
+ */
+using System;
+
+namespace mcev.hello
+{
+    /// <summary>
+    /// A program to print out "Hello World".
+    /// </summary>
+    public class HelloWord
+    {
+        /// <summary>
+        /// Main program.
+        /// </summary>
+        public void main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
